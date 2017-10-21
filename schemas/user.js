3dc1c9e7829373a689c84a1cab6798e2e12571ce
willian <lordhaskhell@gmail.com>
2017-10-21 09:37:33 -0500

    actualiza modulos

--- schemas/user.js
+++ schemas/user.js
@@ -2,8 +2,8 @@
 
 const mongoose=require('mongoose')
 const Schema=mongoose.Schema
-const bcrypt=require('bcrypt-nodejs')
-const crypto=require('crypto')
+//const bcrypt=require('bcrypt-nodejs')
+//const crypto=require('crypto')
 
 const UserSchema=new Schema({
 
