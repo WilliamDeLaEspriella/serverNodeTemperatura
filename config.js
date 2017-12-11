26359a7b61f6da69352e93d4dd224df968207ac9
willian <lordhaskhell@gmail.com>
2017-12-10 20:20:37 -0500

    cambio firebase

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
 	port:process.env.PORT || 3000,
- 	server: process.env.SERVER || 'AAAAISTb-Ls:APA91bEZYT3F8gev4VlrHbcucFMXO-UttqTfFSniwaG8kWBOefHpTUrKoAsyc-Rxu-n8534XTOx8fBSwAXKnZQfb6Qk--G0MaQ7jwDJ01qtl1oZx0JJjMTSCbUfb_k9hMmIXkTaqgE3Z',
+ 	server: process.env.SERVER || 'AAAA5Mjkrgc:APA91bGaUWLx37xAH_HrU7DAUHadCTfkWM3aDtE6T-RLYY68fra_TBP2kBvwOCzP3Gw3-M2l9-pISGt-i9j6VRPVaTnJrJIm5P67VogQnzz_6ehgZGeGs0GyAvulLWkPHQBiCAF6XqWs',
  	db: process.env.MONGODB || 'mongodb://admin:123456789@ds125195.mlab.com:25195/database_calidad_pruebas',
 	SECRET_TOKEN:"SDS64FSDZV458ZXC46ZXC5498ZXC65RF8C4897DV4FSDV654SDCSDV654SDV654SDV6SDV654"
 }         
